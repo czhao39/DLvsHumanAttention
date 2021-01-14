2e104d157ca359da9883831de3f2f7ad030bd908
Haicheng Charles Zhao <czhao39@gmail.com>
2021-01-14 16:21:12 -0500

    Better command-line args for heatmaps directories

--- get_correlation.py
+++ get_correlation.py
@@ -11,11 +11,12 @@ from PIL import Image
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--input_file", "-i", required=True, help="path to a file containing image file names")
-parser.add_argument("--dir", "-d", required=True, action="append", help="a directory of heatmaps (use multiple times for comparing heatmaps)")
+parser.add_argument("--model_heatmaps_dir", "-m", required=True, help="a directory of heatmaps")
+parser.add_argument("--fixation_heatmaps_dir", "-x", required=True, action="append", help="a directory of heatmaps (can use multiple times)")
 parser.add_argument("--size", "-s", type=int, default=32, help="scale heatmaps to this size")
 parser.add_argument("--fig_dir", "-o", help="directory to output comparison figures to")
 parser.add_argument("--agg_fig_dir", "-a", type=str, help="output aggregated figures to this path")
-parser.add_argument("--img_dir", "-m", help="image directory (only used if --fig_dir passed)")
+parser.add_argument("--img_dir", "-d", help="image directory (only used if --fig_dir passed)")
 parser.add_argument("--get_std_err", "-e", action="store_true", help="get correlation standard error by bootstrapping")
 parser.add_argument("--get_rand_distr", "-g", action="store_true", help="get distribution (mean and standard deviation) of random correlations by bootstrapping")
 opt = parser.parse_args()
@@ -24,12 +25,14 @@ opt = parser.parse_args()
 def main():
     N = 500  # sample size for bootstrapping
     dim = (opt.size, opt.size)
-    xi = [[] for _ in range(len(opt.dir))]
+    with open(opt.input_file) as infile:
+        filenames = [line.split()[0] for line in infile.readlines()]
+
+    print("Computing correlations and generating figures...")
+    xi = [[] for _ in range(1 + len(opt.fixation_heatmaps_dir))]
     if opt.agg_fig_dir:
         all_figs = []
         all_corrs = []
-    with open(opt.input_file) as infile:
-        filenames = [line.split()[0] for line in infile.readlines()]
     for fn in filenames:
         im_name = os.path.splitext(fn)[0]
         fn = f"{im_name}.npy"
@@ -37,7 +40,7 @@ def main():
         heatmaps = []
         heatmaps_raveled = []
 
-        for i, d in enumerate(opt.dir):
+        for i, d in enumerate([opt.model_heatmaps_dir] + opt.fixation_heatmaps_dir):
             path = os.path.join(d, fn)
             hm = np.array(Image.fromarray(np.load(path)).resize(dim))
             hm /= hm.sum()
@@ -56,7 +59,7 @@ def main():
 
         hm_ref = cv2.cvtColor(cv2.applyColorMap((heatmaps[0] * 255).astype(np.uint8), cv2.COLORMAP_JET), cv2.COLOR_BGR2RGB) / 255.0
         axs[1].imshow(im_resized*0.6 + hm_ref*0.4)
-        dir_name = os.path.basename(opt.dir[0])
+        dir_name = os.path.basename(opt.model_heatmaps_dir)
         axs[1].set_xlabel(dir_name[:dir_name.rfind("_")])
 
         corrs = []
@@ -66,7 +69,7 @@ def main():
             corr = np.corrcoef(heatmaps_raveled[0], heatmaps_raveled[i])[0, 1]
             corrs.append(corr)
             axs[i+1].set_title(f"Corr: {corr:.3f}")
-            dir_name = os.path.basename(opt.dir[i])
+            dir_name = os.path.basename(opt.fixation_heatmaps_dir[i-1])
             axs[i+1].set_xlabel(dir_name[:dir_name.rfind("_")])
 
         for ax in axs:
@@ -95,11 +98,11 @@ def main():
     if opt.agg_fig_dir:
         # Output aggregated figures sorted by each correlation column
         print("Outputting sorted aggregated figures...")
-        for i in range(1, len(opt.dir)):
-            sorted_figs = sorted(enumerate(all_figs), key=lambda tup: all_corrs[tup[0]][i-1])
+        for i, d in enumerate(opt.fixation_heatmaps_dir):
+            sorted_figs = sorted(enumerate(all_figs), key=lambda tup: all_corrs[tup[0]][i])
             sorted_figs = [tup[1] for tup in sorted_figs]
-            im = Image.fromarray(np.vstack(sorted_figs)).resize((200*(len(opt.dir)+1), 250*len(sorted_figs)))
-            dir_name = os.path.basename(opt.dir[i])
+            im = Image.fromarray(np.vstack(sorted_figs)).resize((200*(2+len(opt.fixation_heatmaps_dir)), 250*len(sorted_figs)))
+            dir_name = os.path.basename(opt.fixation_heatmaps_dir[i])
             map_name = dir_name[:dir_name.rfind("_")]
             outpath = os.path.join(opt.agg_fig_dir, f"{map_name}.png")
             im.save(outpath)
